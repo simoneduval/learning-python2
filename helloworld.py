d091f5d20e12edb99cd6cdcf090e6fca45afd3ee
simone-duval <simone.teresa.duval@gmail.com>
2014-01-15 13:29:26 -0500

    changed HellowWorld.py

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+# learning python commands
+
+print "hello world!"
+
+print "my name is Simone"
+
+print "Let's count some things"
+
+print "Boys", 25 + 30 / 6
+print "Girls", 100-25 * 3 % 4
+
+print "Let's count the class:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
