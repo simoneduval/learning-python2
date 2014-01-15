649e1c79224681c5bd558ce7378089b344e68e92
simone-duval <simone.teresa.duval@gmail.com>
2014-01-15 14:19:01 -0500

    Assignment3.py wrote python script, have to figure out why giving (#,#)

---
+++ assignment3.py
@@ -0,0 +1,15 @@
+age = 22
+salary = 0
+education_years = 4
+experience_years = 2
+
+
+money_now = ((age + 0) + salary) * education_years/ experience_years + 0 
+money_in_5_years = ((age+ 5) + salary) * education_years/ experience_years + 50,000 
+money_in_10_years = ((age + 10) + salary) * education_years / experience_years + 100,000
+
+print "Right now, I have $%d." % money_now
+print money_in_5_years
+print "In 5 years, I will have $%d." % money_in_5_years
+print "In 10 years, I will have $%d." % money_in_10_years
+
